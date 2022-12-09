8575fb90fc81e41ad57873881927437c7961f922
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-12-09 14:17:39 +0800

    millerquest-0.9.1 just works.

--- ruby1.8.7-in-docker.sh
+++ ruby1.8.7-in-docker.sh
@@ -3,5 +3,7 @@
 # docker run -it --name precise ubuntu:12.04
 sed -i -e 's/archive.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 apt-get update
-apt-get install vim ruby
+apt-get install vim ruby wget
 ruby --version
+# millerquest-0.9.1 just works.
+wget https://web.archive.org/web/20110202053521/http://www.beastwithin.org/users/wwwwolf/games/millerquest/millerquest-0.9.1.tar.bz2
